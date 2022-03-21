f8583bd0d14c4039b7846fbad13d09c3c1501311
Denis-zx <49111990+Denis-zx@users.noreply.github.com>
2022-03-21 13:00:36 +0800

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,88 @@
+def contact_point_cal_x(x1,y1,x2,y2,c):
+    k = (float(y2)-y1)/(x2-x1) #斜率
+    yt = y1+ (c-x1)*k
+    return round(yt,1)
+
+def contact_point_cal_y(x1,y1,x2,y2,c):
+    k = (float(x2)-x1)/(y2-y1) #斜率
+    xt = x1+ (c-y1)*k
+    return round(xt,1)
+
+
+def line_spliter_horizon(line_list):
+    #part1,2,3 为分区图像，contact1，2 为交界点
+    new_line_list= {"part1":[],"part2":[],"part3":[],"contact1":[],"contact2":[]}
+    for line in line_list:
+        for i in range(len(line)-1):
+            if line[i] < line[i+1]:
+                rev = False
+                x1,y1 = line[i]
+                x2,y2 = line[i+1]
+            else:
+                rev = True
+                x1,y1 = line[i+1]
+                x2,y2 = line[i]
+
+            #85 contact point 没handle
+            if x1<85:
+                if x2 < 85: #CASE1 1-1
+                    new_line_list["part1"].append([line[i],line[i+1]])
+
+                else:#CASE2 1-2
+                    #处理85分界线
+                    y_85 = contact_point_cal_x(x1,y1,x2,y2,85)
+                    new_line_list["contact1"].append(y_85)
+                    if not rev:
+                        new_line_list["part1"].append([line[i],(85,y_85)])
+                    else:
+                        new_line_list["part1"].append([line[i+1],(85,y_85)])
+                    
+
+                    if 170<=x2:
+                        #处理170分界线
+                        y_170 = contact_point_cal_x(x1,y1,x2,y2,170)
+                        new_line_list["contact2"].append(y_170)
+                        if not rev:
+                            new_line_list["part1"].append([line[i],(85,y_85)])
+                            new_line_list["part2"].append([(85,y_85),(170,y_170)])
+                            new_line_list["part3"].append([(170,y_170),line[i+1]])
+                        else:
+                            new_line_list["part1"].append([line[i+1],(85,y_85)])
+                            new_line_list["part2"].append([(85,y_85),(170,y_170)])
+                            new_line_list["part3"].append([(170,y_170),line[i]])
+                    else:
+                        if not rev:   
+                            new_line_list["part2"].append([(85,y_85),line[i+1]])
+                        else:
+                            new_line_list["part2"].append([(85,y_85),line[i]])
+                            
+            elif 85<= x1 <170: 
+                if x2 < 170: #CASE4 2-2
+                    new_line_list["part2"].append([line[i],line[i+1]])
+
+                else: #CASE5 2-3
+                    y_170 = contact_point_cal_x(x1,y1,x2,y2,170)
+                    new_line_list["contact2"].append(y_170)
+                    if not rev:
+                        new_line_list["part2"].append([line[i],(170,y_170)])
+                        new_line_list["part3"].append([(170,y_170),line[i+1]])
+                    else:
+                        new_line_list["part2"].append([line[i+1],(170,y_170)])
+                        new_line_list["part3"].append([(170,y_170),line[i]])
+
+                    
+            else:#CASE6 3-3
+                new_line_list["part3"].append([line[i],line[i+1]])
+            
+    new_line_list["contact1"] = sorted(new_line_list["contact1"])
+    new_line_list["contact2"] = sorted(new_line_list["contact2"])
+
+    return new_line_list
+
+
+
+                        
+                        
+
+                        
+                    
---
+++ test.py
@@ -0,0 +1,76 @@
+import json
+from modifier import modifier
+from PIL import Image, ImageDraw
+from combiner import selector
+
+def draw(image,canvas,p1,p2,p3):
+   for polyline in p1:
+      canvas.line([tuple(item) for item in polyline],fill=(0, 0, 0),width=3)
+
+   for polyline in p2:
+      canvas.line([tuple(item) for item in polyline],fill=(0, 0, 0),width=3)
+
+   for polyline in p3:
+      canvas.line([tuple(item) for item in polyline],fill=(0, 0, 0),width=3)
+
+   # display image
+   image.show()
+
+
+
+
+
+# read ndjson lines
+lines = open('horizon_partition/horizon_partition_ant.json','r').readlines()
+# 中间要插入的文件类
+target_filename = 'horizon_partition/horizon_partition_airplane.json'
+lines2 = open(target_filename,'r').readlines()
+# grab the first line, JSON parse it and fetch the 'drawing' array
+
+
+
+#initial picture
+image1 = json.loads(lines[0])
+
+#选合适中间插入的图
+mid_image_num = selector(image1,target_filename)
+print(mid_image_num)
+
+
+
+raw_drawing = image1
+raw_drawing2 = json.loads(lines2[mid_image_num])
+
+#part2 = modifier(image1["contact1"],image1["contact2"],raw_drawing2)
+
+#print('before',raw_drawing)
+# zip x,y coordinates for each point in every polyline
+#polylines = [list(zip(polyline[0], polyline[1])) for polyline in raw_drawing]
+# notice how the data is shuffled to (x1,y1),(x2,y2) order
+#print('after',polylines)
+#polylines = [sorted(polyline)for polyline in polylines]
+#print('sort',polylines)
+
+# make a new image
+pil_img1 = Image.new("RGB", (240, 270), (255,255,255))
+pil_img2 = Image.new("RGB", (240, 270), (255,255,255))
+pil_imgc = Image.new("RGB", (240, 270), (255,255,255))
+# get a drawing context
+cav1 = ImageDraw.Draw(pil_img1)
+cav2 = ImageDraw.Draw(pil_img2)
+cavc = ImageDraw.Draw(pil_imgc)
+
+new_line_dict2 = raw_drawing2
+
+new_line_dict = raw_drawing
+# render each polyline
+
+# original graph 1
+draw(pil_img1,cav1,raw_drawing["part1"],raw_drawing["part2"],raw_drawing["part3"])
+
+# original matched graph
+draw(pil_img2,cav2,raw_drawing2["part1"],raw_drawing2["part2"],raw_drawing2["part3"])
+
+# combined graph
+draw(pil_imgc,cavc,raw_drawing["part1"],raw_drawing2["part2"],raw_drawing["part3"])
+
