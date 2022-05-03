1c6d75a2e6fc520babf61596a04d753b420f8f34
denis-zx <denis-zx@hotmail.com>
2022-05-03 23:13:04 +0800

    delete and dependency

--- modifier.py
+++
@@ -1,8 +0,0 @@
-
-def modifier(bridge_pt1, bridge_pt2,image):
-    part2 = image["part2"].sort()
-    for i in range(len(bridge_pt1)):
-        part2[i][1] = bridge_pt1[i]
-    for i in range(len(bridge_pt2)):
-        part2[len(part2)-len(bridge_pt2)+i][1] = bridge_pt2[i]
-    return part2
